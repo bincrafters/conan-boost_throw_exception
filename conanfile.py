15514e01bdf330825ed6d34140af4f3878776355
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:27:16 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BoostThrow_ExceptionConan(ConanFile):
     version = "1.64.0"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-throw_exception"
-    source_url = "https://github.com/boostorg/throw_exception"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["throw_exception"]
@@ -13,9 +12,12 @@ class BoostThrow_ExceptionConan(ConanFile):
                       "Boost.Assert/1.64.0@bincrafters/testing"
     
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def package(self):
         for lib_short_name in self.lib_short_names:
