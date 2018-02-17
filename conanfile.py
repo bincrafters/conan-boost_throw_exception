f5807acf55d0d6240217ff6abc05f38a49b5524c
Jerry W <jerrywiltse@gmail.com>
2018-02-17 09:39:00 -0500

    merge from testing

--- conanfile.py
+++ conanfile.py
@@ -17,9 +17,9 @@ class BoostThrow_ExceptionConan(ConanFile):
         self.info.header_only()
     
     requires = (
-        "boost_package_tools/1.65.1@bincrafters/testing",
-        "boost_config/1.65.1@bincrafters/testing",
-        "boost_assert/1.65.1@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/stable",
+        "boost_config/1.65.1@bincrafters/stable",
+        "boost_assert/1.65.1@bincrafters/stable"
     )
 
     # BEGIN
@@ -27,7 +27,7 @@ class BoostThrow_ExceptionConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.65.1@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
