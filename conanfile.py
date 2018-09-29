1d17eb5ac4c785779df6c73a4d599c9a9fb3d23b
Jerry W <jerrywiltse@gmail.com>
2018-09-29 17:15:19 -0400

    move to python_requires strategy

--- conanfile.py
+++ conanfile.py
@@ -1,59 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import python_requires
 
 
-class BoostThrow_ExceptionConan(ConanFile):
+base = python_requires("boost_base/1.67.0@bincrafters/testing")
+
+class BoostThrow_ExceptionConan(base.BoostBaseConan):
     name = "boost_throw_exception"
-    version = "1.67.0"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    exports = ["LICENSE.md"]
-    lib_short_names = ["throw_exception"]
-    is_header_only = True
-
-    def package_id_additional(self):
-        self.info.header_only()
-
-    requires = (
-        "boost_assert/1.67.0@bincrafters/testing",
-        "boost_config/1.67.0@bincrafters/testing",
-        "boost_package_tools/1.67.0@bincrafters/testing"
-    )
-
-    # BEGIN
-
     url = "https://github.com/bincrafters/conan-boost_throw_exception"
-    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
-    license = "BSL-1.0"
-    short_paths = True
-    build_requires = "boost_generator/1.67.0@bincrafters/testing"
+    lib_short_names = ["throw_exception"]
+    header_only_libs = ["throw_exception"]
+    b2_requires = [
+        "boost_assert",
+        "boost_config",
+    ]
 
-    def package_id(self):
-        getattr(self, "package_id_additional", lambda:None)()
 
-    def source(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.source(self)
-        getattr(self, "source_additional", lambda:None)()
-
-    def build(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.build(self)
-        getattr(self, "build_additional", lambda:None)()
-
-    def package(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package(self)
-        getattr(self, "package_additional", lambda:None)()
-
-    def package_info(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package_info(self)
-        getattr(self, "package_info_additional", lambda:None)()
-
-    # END
